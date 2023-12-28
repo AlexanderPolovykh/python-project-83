be563d875e654b21b358be69265b1a6d4112f845
polovykh <polovykh@mail.ru>
2023-12-28 16:35:08 +0700

    Step 1 (2)

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,14 +1,19 @@
 from flask import Flask
-from dotenv import dotenv_values
-import json
+from dotenv import load_dotenv
+import os
 
+# from dotenv import dotenv_values
+# import json
+
+load_dotenv()
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-    config = dotenv_values('.env')
-    json_str = json.dumps(config)
-    return f"<p>Hello, World and Earth!<br/><br/>Configuration is {json_str}</p>"
-    # return f"<p>Hello, World and Earth! password={config['PASSWORD']}</p>"
+    # config = dotenv_values('.env')
+    # json_str = json.dumps(config)
+    port = os.getenv("TCP_PORT")
+    # return f"<p>Hello, World and Earth!<br/><br/>Configuration is {json_str}</p>"
+    return f"<p>Hello, World and Earth!<br/><br/>TCP_PORT = {port}</p>"
